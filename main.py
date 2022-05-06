851228951e2c342dfb810452ef7df7fab21cf3a7
caddy <haitham.seelawi@gmail.com>
2022-05-06 22:02:53 +0100

    fixes dvc issue

--- main.py
+++ main.py
@@ -19,9 +19,7 @@ cat_features = [
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    os.system("dvc pull")
 
 model = load('./model/classifier.joblib')
 encoder = load('./model/encoder.joblib')
