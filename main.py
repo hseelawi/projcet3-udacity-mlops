544591cc830eaf3260baf273833392aaf0021c2f
caddy <haitham.seelawi@gmail.com>
2022-05-11 11:02:52 +0100

    fixes heroku

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ cat_features = [
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    os.system("dvc pull")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 model = load('./model/classifier.joblib')
 encoder = load('./model/encoder.joblib')
